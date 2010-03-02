8501512bea670ec3c6063c6ff2f28887370d1487
Matt Behrens <askedrelic@gmail.com>
2010-03-01 20:55:49 -0500

    Mike's script for creating a new post, still needs work

---
+++ newpost.py
@@ -0,0 +1,32 @@
+ #!/usr/bin/env python
+import re
+import sys
+from datetime import datetime
+ 
+title = sys.argv[1]
+ 
+NON_CHAR = re.compile(r'[^a-z0-9-]+')
+def prettify_title(title):
+    title = title.lower()
+    title = NON_CHAR.sub('-', title)
+    return title
+ 
+date_format = "%Y/%m/%d %H:%M:%S"
+file_date_format = "%Y-%m-%d"
+now = datetime.now()
+ 
+fname = "_posts/%s-%s.rst" % (now.strftime(file_date_format), prettify_title(title))
+ 
+new_file = open(fname, 'w')
+ 
+template = """---
+categories: Tech
+date: %s
+tags: ''
+title: %s
+""" % (now.strftime(date_format), title)
+ 
+new_file.write(template)
+new_file.close()
+ 
+print "new post '%s' created" % fname 
