5e6f682a291e91ba782af1647f369997e1094767
Matt Behrens <askedrelic@gmail.com>
2010-04-05 14:30:37 -0400

    updating post script, relative links on the index

--- _newpost.py
+++ _newpost.py
@@ -15,15 +15,16 @@ date_format = "%Y/%m/%d %H:%M:%S"
 file_date_format = "%Y-%m-%d"
 now = datetime.now()
  
-fname = "_posts/%s-%s.rst" % (now.strftime(file_date_format), prettify_title(title))
+fname = "_posts/%s-%s.markdown" % (now.strftime(file_date_format), prettify_title(title))
  
 new_file = open(fname, 'w')
  
 template = """---
-categories: Tech
+categories: Life
 date: %s
 tags: ''
 title: %s
+---
 """ % (now.strftime(date_format), title)
  
 new_file.write(template)
