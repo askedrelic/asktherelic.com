3b670dc1419c7db911653fefb35f90e3b1fbd776
Matt Behrens <askedrelic@gmail.com>
2010-09-06 00:06:45 -0400

    fixing whitespace :<

--- _newpost.py
+++ _newpost.py
@@ -2,23 +2,23 @@
 import re
 import sys
 from datetime import datetime
- 
+
 title = sys.argv[1]
- 
+
 NON_CHAR = re.compile(r'[^a-z0-9-]+')
 def prettify_title(title):
     title = title.lower()
     title = NON_CHAR.sub('-', title)
     return title
- 
+
 date_format = "%Y/%m/%d %H:%M:%S"
 file_date_format = "%Y-%m-%d"
 now = datetime.now()
- 
+
 fname = "_posts/%s-%s.markdown" % (now.strftime(file_date_format), prettify_title(title))
- 
+
 new_file = open(fname, 'w')
- 
+
 template = """---
 categories: Life
 date: %s
@@ -26,8 +26,8 @@ tags: ''
 title: %s
 ---
 """ % (now.strftime(date_format), title)
- 
+
 new_file.write(template)
 new_file.close()
- 
-print "new post '%s' created" % fname 
+
+print "new post '%s' created" % fname
