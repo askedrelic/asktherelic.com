4866953aa28e7c6cdefa72db4e93324e57bf5ef8
Matt Behrens <askedrelic@gmail.com>
2010-10-23 20:35:38 -0400

    cleanup. don't need publish function

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,8 @@ def package():
     with cd("_site/"):
         local("tar zcvf ../blog.tgz --exclude '.DS_Store' --exclude '.gitignore' --exclude 'fabfile*' *")
 
-def new_post(post_name):
+def new(post_name):
+    """new post. requires post name arguement"""
     NON_CHAR = re.compile(r'[^a-z0-9-]+')
     def prettify_title(title):
         title = title.lower()
@@ -24,34 +25,18 @@ def new_post(post_name):
     new_file = open(fname, 'w')
 
     template = """---
-    categories: Life
-    date: %s
-    tags: ''
-    title: %s
-    draft: true
-    ---
+categories: Life
+date: %s
+tags: ''
+title: %s
+draft: true
+---
     """ % (now.strftime(date_format), post_name)
 
     new_file.write(template)
     new_file.close()
 
-    print "new post '%s' created" % fname
-
-#def publish(post_name):
-#    """
-#    sed -e's/draft: true/draft: false'
-#    """
-#    from datetime import datetime
-#
-#    now = datetime.now()
-#    old_post_date = "2000/01/01 00:00:01"
-#    post_date = now.strftime('%Y/%m/%d %H:%M:%S')
-#    post_format = "_posts/0000_%s.rst"
-#    post = post_format % "_".join(post_name.split(" "))
-#    local("sed -i -e's!%s!%s!' %s" % (old_post_date, post_date, post))
-
-#def install():
-#    local("sudo pip install -r requirements.txt")
+    print "New draft post created: \n%s\n" % fname
 
 def test():
     local("blogofile build")
