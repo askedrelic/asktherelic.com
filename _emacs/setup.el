153c6d0dec3f24b3147e1582327d3456dd66756d
Matt Behrens <askedrelic@gmail.com>
2012-10-17 00:40:02 -0700

    when did i add this

--- _emacs/setup.el
+++
@@ -1,5 +0,0 @@
-;;; add load path for orgmode
-;;;    and intialize code and prehandling routine
-(setq load-path (cons "~/path/to/orgdir/lisp" load-path))
-(require 'org-install)
-(add-to-list 'auto-mode-alist '("\.org$" . org-mode))
